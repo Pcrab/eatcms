447e212790d4c2475506f9893f9d7908cc45dc0d
Pcrab <pcrab@pcrab.xyz>
2022-05-01 20:56:03 +0800

    change url generator and add test constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,9 +1,13 @@
+const remoteBaseUrl = "http://127.0.0.1/";
+
 const CONSTANTS = {
-  remoteBaseUrl: "http://127.0.0.1/",
-  loginUrl: "",
-  registerUrl: "",
-  resetPwdUrl: "",
-  createUserUrl: "",
+  loginUrl: remoteBaseUrl + "",
+  resetPwdUrl: remoteBaseUrl + "",
+  createUserUrl: remoteBaseUrl + "",
+
+  testing: true,
+  testingLogin: false,
+  testingAdmin: true,
 
   copyRight: "TEST COPYRIGHT 2022",
 };
